de6fb787eaee41e99cb40d79de0fe7a4c7f492e3
tobeqc <tobeqc@tobeqcui-iMac.local>
2013-04-23 16:43:49 +0900

    Initial Commit

---
+++ 13RI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  13RI
+//
+//  Created by tobeqc on 13. 4. 23..
+//  Copyright (c) 2013년 tobeqc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 13RI/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  13RI
+//
+//  Created by tobeqc on 13. 4. 23..
+//  Copyright (c) 2013년 tobeqc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
